ff6e5ae7ac9772ee7858309650d44b0e2b087204
AnujMishra <anujm77000@gmail.com>
2024-08-31 16:58:00 +0530

    version 3

---
+++ Watchlist_collection/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from  Watchlist_collection.models import WatchList, StreamPlatform, Review
+
+admin.site.register(WatchList)
+admin.site.register(StreamPlatform)
+admin.site.register(Review)
---
+++ Watchlist_collection/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WatchlistCollectionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Watchlist_collection'
